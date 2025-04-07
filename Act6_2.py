9970008bc02d08063beee5cdf6c72bab2410de6e
Ismaelduranm <A01731393@tec.mx>
2025-04-07 00:20:27 -0600

    Funcion_info(isma)

--- Act6_2.py
+++ Act6_2.py
@@ -8,3 +8,13 @@ def multi():
     print(a*b*c)
 
 multi()
+
+def funcion_info(ruta_archivo)
+    import pandas as pd
+    df = pd.read_csv(ruta_archivo)
+    return {
+        "filas_columnas": df.shape,
+        "columnas": df.columns.tolist(),
+        "tipos_dato": df.dtypes,
+        "estadisticas": df.describe(include='all')
+    }
